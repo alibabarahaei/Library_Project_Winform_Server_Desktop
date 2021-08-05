24fd8b6cfad2f92675013716c12a0330b606ac8f
Ali Babarahaei <Alibabarahaei@gmail.com>
2021-08-05 11:02:42 +0430

    library project server

--- WinFormServer/MyForm.h
+++ WinFormServer/MyForm.h
@@ -10,6 +10,7 @@ namespace WinFormServer {
 	using namespace System;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
+
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
